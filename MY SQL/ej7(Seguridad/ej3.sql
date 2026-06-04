fe7b3b4d48f8e0984972d7749083c5cd27ab026f
= <=>
2026-06-04 10:18:01 -0300

    feat: ej 3

---
+++ MY SQL/ej7(Seguridad/ej3.sql
@@ -0,0 +1,8 @@
+
+
+GRANT 'lector' TO 'analistaStock'@'localhost';
+GRANT 'stocker' to 'gestorProducto'@'localhost';
+GRANT 'stocker' to 'usuarioProducto'@'localhost';
+GRANT 'dev' to 'desarrollo'@'localhost';
+
+GRANT 'admin' to 'desarrollo'@'localhost';
